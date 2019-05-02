016b21fa933838aefc1451881d1007ff9278e22e
Tim.C <chih-ting.chen@cgu.edu>
2019-05-01 23:17:11 -0700

    allow the data to add rows as many as we want

--- data.py
+++ data.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import numpy as np
 
-L = ['Tim',10,10,10,10,10,10,10,5,6,7,200,30,230]  # this should create by the program
-data = pd.read_csv('Data.csv') #read the current file
-newdata = data.append(pd.Series(L,index=data.columns),ignore_index=True) #combine to the file
+L = ['Tim',10,10,10,10,10,10,10,5,6,7,200,30,230] # this should create by the program
 
-newdata.to_csv('Data.csv') #generate the file
+data = pd.read_csv('Data.csv')
+data = data.drop(data.columns[0],axis=1)
+newdata = data.append(pd.Series(L, index= data.columns),ignore_index=True)
+# print(newdata)
+newdata.to_csv('Data.csv')
+# print(data)
